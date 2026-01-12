30ad0fb661f25945354e567a62f129208faaa35f
omkarr021 <osdoifode2005@gmail.com>
2026-01-13 01:43:47 +0530

    no appp

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const {userRouter} = require('./routes/user');
 const {courseRouter} = require('./routes/course');
 
 const app = express();
-app.use("/user", userRouter);
+
+
+git aapp.use("/user", userRouter);
 app.use("/course", courseRouter);
 
 
