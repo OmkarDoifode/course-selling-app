c4b7a4143828e05390982c679fdc7e4a04aa915c
omkarr021 <osdoifode2005@gmail.com>
2026-01-13 01:15:01 +0530

    modified index.js

--- index.js
+++ index.js
@@ -3,22 +3,22 @@ const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
 const app = express();
 
-app.post("/signup", async (req, res)=>{
+app.post("/user/signup", async (req, res)=>{
     const username = req.body.username;
     const password = req.body.password;
     const name = req.body.name;
 
 })
-app.post("/login", async(req, res)=>{
+app.post("/user/signin", async(req, res)=>{
 
 })
-app.get("/purchase-course", (req, res) =>{
+app.get("users/purchases", (req, res)=>{
 
 })
-app.get("/my-courses", (req, res)=>{
+app.get("/courses", (req,res)=>{
 
 })
-app.get("all-courses", (req,res)=>{
+app.post("/course/purchase", (req, res) => {
     
 })
 
