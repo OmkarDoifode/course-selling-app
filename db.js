a9a3677ec0a74748545387afc786aaa0d52930d2
omkarr021 <osdoifode2005@gmail.com>
2026-01-15 18:10:12 +0530

    written Schema

---
+++ db.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const ObjectId = mongoose.ObjectId;
+
+const userSchema = new Schema({
+    email: {type: String, unique: true},
+    password: String,
+    firstName: String,
+    lastName: String
+})
+const courseSchema = new Schema({
+    title: String,
+    description: String,
+    price: Number,
+    imageUrl: String,
+    creatorId: ObjectId
+})
+const adminSchema = new Schema({
+    email: {type: String, unique: true},
+    password: String,
+    firstName: String,
+    lastName: String
+})
+const purchaseSchema = new Schema({
+    courseId: ObjectId,
+    userId: ObjectId
+})
+
+const userModel = mongoose.Schema("user", userSchema);
+const courseModel = mongoose.Schema("course", courseSchema);
+const adminModel = mongoose.Schema("admin", adminSchema);
+const purchaseModel = mongoose.Schema("purchase", purchaseSchema);
